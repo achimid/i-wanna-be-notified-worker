7ee5b39e697f365fa338c5ed82781be33acb70be
Lourran <achimidd@gmail.com>
2020-09-27 08:32:15 -0300

    Adicionando dash de metrica

--- producer-test.js
+++ producer-test.js
@@ -7,21 +7,119 @@ const main = async () => {
     const { execute } = require('./src/execution/execution-scraper')
     const { startExecution } = require('./src/execution/execution-service')
 
-    const data = {
+    // FenixSub
+    let data = {
         "scriptContent": [
-            "[...document.querySelectorAll('#menu2 > table tr')].map(i => i.innerText.trim()).splice(1)[0].split('/msg')[0].trim()"
+            "document.querySelector('h1').innerText.replace('SÉRIES\n', '') + ' - ' + [...document.querySelectorAll('.nome-ep')].pop()?.innerText",
+            "[...document.querySelectorAll('#lista-posts .content > .link')].slice(0, 5).map(i => i.href)"            
         ],
         "_id": "5f6f73fb477f52001edf4fec",
-        "url": "https://packs.ansktracker.net/",
-        "name": "AnimeNSK",
-        "scriptTarget": "[...document.querySelectorAll('#menu2 > table tr')].map(i => i.innerText.trim())",
+        "url": "https://fenixfansub.com/",
+        "name": "FenixSub",
+        "scriptTarget": "[...document.querySelectorAll('#lista-posts .content')].slice(0, 15).splice(0, 5).map(i => i.innerText)",
         "regularity": "*/15 * * * *",
         "options": {
-            "levelMax": 0,
+            "levelMax": 1,
             "notifyChange": true,
             "notifyUniqueChange": true
         },
     }
+
+
+    // Animes Vision
+    data = {
+        "scriptContent": [
+            "document.querySelector('h1').innerText.replace('SÉRIES\n', '') + ' - ' + [...document.querySelectorAll('.nome-ep')].pop()?.innerText",
+            "[...document.querySelectorAll('#lista-posts .content > .link')].slice(0, 5).map(i => i.href)"            
+        ],
+        "_id": "5f6f73fb477f52001edf4fec",
+        "url": "https://animesvision.biz/",
+        "name": "FenixSub",
+        "scriptTarget": "[...document.querySelectorAll('#lista-posts .content')].slice(0, 15).splice(0, 5).map(i => i.innerText)",
+        "regularity": "*/15 * * * *",
+        "options": {
+            "levelMax": 1,
+            "notifyChange": true,
+            "notifyUniqueChange": true
+        },
+    }
+
+    // InfiniteSub
+    data = {
+        "scriptContent": [
+            "document.querySelector('h1').innerText.replace('SÉRIES\n', '') + ' - ' + [...document.querySelectorAll('.nome-ep')].pop()?.innerText",
+            "[...document.querySelectorAll('#lista-posts .content > .link')].slice(0, 5).map(i => i.href)"            
+        ],
+        "_id": "5f6f73fb477f52001edf4fec",
+        "url": "https://infinitefansub.com/",
+        "name": "FenixSub",
+        "scriptTarget": "[...document.querySelectorAll('#lista-posts .content')].slice(0, 15).splice(0, 5).map(i => i.innerText)",
+        "regularity": "*/15 * * * *",
+        "options": {
+            "levelMax": 1,
+            "notifyChange": true,
+            "notifyUniqueChange": true
+        },
+    }
+
+    // Kyoshiro Fansub
+    data = {
+        "scriptContent": [
+            "document.querySelector('h1').innerText.replace('SÉRIES\n', '') + ' - ' + [...document.querySelectorAll('.nome-ep')].pop()?.innerText",
+            "[...document.querySelectorAll('#lista-posts .content > .link')].slice(0, 5).map(i => i.href)"            
+        ],
+        "_id": "5f6f73fb477f52001edf4fec",
+        "url": "https://www.kyoshirofansub.com/",
+        "name": "FenixSub",
+        "scriptTarget": "[...document.querySelectorAll('#lista-posts .content')].slice(0, 15).splice(0, 5).map(i => i.innerText)",
+        "regularity": "*/15 * * * *",
+        "options": {
+            "levelMax": 1,
+            "notifyChange": true,
+            "notifyUniqueChange": true
+        },
+    }
+
+    // HD Subs
+    data = {
+        "scriptContent": [
+            "document.querySelector('h1').innerText.replace('SÉRIES\n', '') + ' - ' + [...document.querySelectorAll('.nome-ep')].pop()?.innerText",
+            "[...document.querySelectorAll('#lista-posts .content > .link')].slice(0, 5).map(i => i.href)"            
+        ],
+        "_id": "5f6f73fb477f52001edf4fec",
+        "url": "https://hdsubs.org/",
+        "name": "FenixSub",
+        "scriptTarget": "[...document.querySelectorAll('#lista-posts .content')].slice(0, 15).splice(0, 5).map(i => i.innerText)",
+        "regularity": "*/15 * * * *",
+        "options": {
+            "levelMax": 1,
+            "notifyChange": true,
+            "notifyUniqueChange": true
+        },
+    }
+    
+    https://subvision.org/
+    https://test-kun.com
+    http://karinsensei.com/
+    https://oldagesubs.com/
+    https://kawaiiotome.com.br/
+    https://www.tenroufansub.com/
+    http://absolutesub.fansub.com.br/
+    https://elite.fansubs.com.br/
+    https://boteco.fansubs.com.br/
+    https://katawaredoki.fansubs.com.br/
+    https://nadjafansub.home.blog/
+    https://kallingfansub.wordpress.com/
+    https://kirinashi.fansubs.com.br/
+    https://mastercorpsfansubbers.blogspot.com/
+    https://sakuraanimes.com/
+    https://animeshouse.net/
+    https://www.anitube.site/
+    https://isekaisubs.com/
+    https://shinmeikai.net/*
+
+
+    
 
     startExecution(data)
         .then(console.log)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+require('appmetrics-dash').attach()
 
 const browserInit = require('./config/puppeteer')
 const healthcheck = require('./config/healthcheck')
